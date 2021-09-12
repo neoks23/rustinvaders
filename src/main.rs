4a07149a32de5ae7098a1973d1677789e8980503
Koen Sampers <57813986+neoks23@users.noreply.github.com>
2021-09-12 20:18:23 +0200

    V4 Enemy spawn system

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 // #![allow(unused)]
 
 mod player;
+mod enemy;
 use bevy::prelude::*;
-use crate::player::PlayerPlugin;
+use crate::{enemy::EnemyPlugin, player::PlayerPlugin};
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const LASER_SPRITE: &str = "laser_a_01.png";
+const ENEMY_SPRITE: &str = "enemy_a_01.png";
 const TIME_STEP: f32 = 1. / 60.;
 const SCALE: f32 = 0.5;
 
 pub struct Materials{
     player: Handle<ColorMaterial>,
     laser: Handle<ColorMaterial>,
+    enemy: Handle<ColorMaterial>,
 }
 struct WinSize{
+    #[allow(unused)]
     w: f32,
     h: f32,
 }
+struct ActiveEnemies(u32);
 
 struct Player;
 struct PlayerReadyFire(bool);
 struct Laser;
+
+struct Enemy;
 
 struct Speed(f32);
 impl Default for Speed {
@@ -38,8 +45,10 @@ fn main() {
             height: 676.,
             ..Default::default()
         })
+        .insert_resource(ActiveEnemies(0))
         .add_plugins(DefaultPlugins)
         .add_plugin(PlayerPlugin)
+        .add_plugin(EnemyPlugin)
         .add_startup_system(setup.system())
         .run();
 }
@@ -57,6 +66,7 @@ fn setup(mut commands: Commands,
     commands.insert_resource(Materials{
         player: materials.add(asset_server.load(PLAYER_SPRITE).into()),
         laser: materials.add(asset_server.load(LASER_SPRITE).into()),
+        enemy: materials.add(asset_server.load(ENEMY_SPRITE).into()),
     });
     commands.insert_resource(WinSize{w: window.height(), h: window.width()});
 
